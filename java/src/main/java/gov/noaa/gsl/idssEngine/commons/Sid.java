6d57ba5c0c9e5bc6a355717cc87bae45497dccbd
Geary Layne <geary.j.layne@noaa.gov>
2022-09-20 12:51:24 -0600

    added hashCode(), equals(), and toString()

--- java/src/main/java/gov/noaa/gsl/idssEngine/commons/Sid.java
+++ java/src/main/java/gov/noaa/gsl/idssEngine/commons/Sid.java
@@ -6,12 +6,14 @@
 *********************************************************************************/
 package gov.noaa.gsl.idssEngine.commons;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
 import gov.noaa.gsd.fiqas.util.time.DateTimeFactory;
+import gov.noaa.gsl.idssEngine.riskProc.RiskProcessor;
 
 public class Sid {
     
@@ -34,5 +36,28 @@ public class Sid {
         this(key, originator, DateTimeFactory.newInstance(issueDt));
     }
     
+    @Override
+    public int hashCode() {
+        return Objects.hash(issueDt, key, originator);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if( this == obj )
+            return true;
+        if( obj == null )
+            return false;
+        if( getClass() != obj.getClass() )
+            return false;
+        Sid other = (Sid) obj;
+        return Objects.equals(issueDt, other.issueDt)
+                && Objects.equals(key, other.key)
+                && Objects.equals(originator, other.originator);
+    }
+    
+    @Override
+    public int toString() {
+        return String.format("Sid(%s, %s, %s)", key, originator, issueDt);
+    }
     
 }
