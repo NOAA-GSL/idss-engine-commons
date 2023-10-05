6dcdc56caa12afb30ae3424651c3c55a2d3e3169
Geary Layne <geary.j.layne@noaa.gov>
2023-10-05 07:22:27 -0600

    added tests for bad criteria

--- python/idsse_common/test/test_validate_schema.py
+++ python/idsse_common/test/test_validate_schema.py
@@ -29,6 +29,99 @@ def data_request_validator() -> Validator:
     return get_validator(schema_name)
 
 
+@fixture
+def criteria_validator() -> Validator:
+    schema_name = 'criteria_schema'
+    return get_validator(schema_name)
+
+
+@fixture
+def criteria_message() -> dict:
+    return {
+        "corrId": {
+            "originator": "IDSSe",
+            "uuid": "4899d220-beec-467b-a0e6-9d215b715b97",
+            "issueDt": "2022/10/07 14:00:00"
+        },
+        "issueDt": "2022/10/07 14:00:00",
+        "location": {
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {
+                        "name": "The spot",
+                        "radius": 3.00
+                    },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            -106.62312540068922,
+                            34.964261450738306
+                        ]
+                    }
+                }
+            ]
+        },
+        "validDt": {
+            "start": "2022/10/08 0:00:00",
+            "end": "2022/10/08 12:00:00"
+        },
+        "conditions": [
+            {
+                "severity": "MODERATE",
+                "combined": "A AND B",
+            },
+        ],
+        "data": {
+            "A": {
+                "arealPercentage": 0,
+                "duration": 0,
+                "product": {
+                    "fcst": [
+                        "NBM"
+                    ]
+                },
+                "field": "DEW POINT",
+                "units": "Fahrenheit",
+                "relational": "LESS THAN",
+                "thresh": 60,
+                "mapping": {
+                    "min": 35.0,
+                    "max": 75.0,
+                    "clip": "true"
+                }
+            },
+            "B": {
+                "arealPercentage": 0,
+                "duration": 0,
+                "product": {
+                    "fcst": [
+                        "NBM"
+                    ]
+                },
+                "field": "RELATIVE HUMIDITY",
+                "units": "PERCENT",
+                "relational": "GREATER THAN",
+                "thresh": 30,
+                "mapping": {
+                    "min": 0.0,
+                    "max": 75.0,
+                    "clip": "true"
+                }
+            }
+        },
+        "tags": {
+            "values": [
+            ],
+            "keyValues": {
+                "name": "Abq Rain",
+                "nwsOffice": "BOU"
+            }
+        }
+    }
+
+
+
 def test_validate_das_issue_request(available_data_validator: Validator):
     message = {'sourceType': 'issue',
                'sourceObj': {'product': 'NBM.AWS.GRIB',
@@ -183,7 +276,7 @@ def test_validate_das_opr_with_single_source_request(data_request_validator: Val
 
 
 def test_validate_das_bad_opr_with_single_source_request(data_request_validator: Validator):
-    # this is an example of a unit conversion operator, the operator itself is not be validated
+    # this is an example of a unit conversion operator, the operator itself is not validated
     message = {
         'sourceType': 'units',
         'sourceObj': {
@@ -205,7 +298,7 @@ def test_validate_das_bad_opr_with_single_source_request(data_request_validator:
 
 
 def test_validate_das_opr_with_multi_sources_request(data_request_validator: Validator):
-    # this is an example of a logical join operator, the operator itself is not be validated
+    # this is an example of a logical join operator, the operator itself is not validated
     message = {
         "sourceType": "join",
         "sourceObj": {
@@ -324,132 +417,37 @@ def test_validate_das_bad_opr_with_multi_sources_request(data_request_validator:
         data_request_validator.validate(message)
 
 
-def test_validate_criteria():
-    message = {
-        "corrId": {
-            "originator": "IDSSe",
-            "uuid": "4899d220-beec-467b-a0e6-9d215b715b97",
-            "issueDt": "2022/10/07 14:00:00"
-        },
-        "issueDt": "2022/10/07 14:00:00",
-        "location": {
-            "features": [
-                {
-                    "type": "Feature",
-                    "properties": {
-                        "name": "The spot",
-                        "radius": 3.00
-                    },
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [
-                            -106.62312540068922,
-                            34.964261450738306
-                        ]
-                    }
-                }
-            ]
-        },
-        "validDt": {
-            "start": "2022/10/08 0:00:00",
-            "end": "2022/10/08 12:00:00"
-        },
-        "conditions": [
-            {
-                "severity": "MODERATE",
-                "combined": "A AND B",
-            },
-        ],
-        "data": {
-            "A": {
-                "arealPercentage": 0,
-                "duration": 0,
-                "product": {
-                    "fcst": [
-                        "NBM"
-                    ]
-                },
-                "field": "DEW POINT",
-                "units": "Fahrenheit",
-                "relational": "LESS THAN",
-                "thresh": 60,
-                "mapping": {
-                    "min": 35.0,
-                    "max": 75.0,
-                    "clip": "true"
-                }
-            },
-            "B": {
-                "arealPercentage": 0,
-                "duration": 0,
-                "product": {
-                    "fcst": [
-                        "NBM"
-                    ]
-                },
-                "field": "RELATIVE HUMIDITY",
-                "units": "PERCENT",
-                "relational": "GREATER THAN",
-                "thresh": 30,
-                "mapping": {
-                    "min": 0.0,
-                    "max": 75.0,
-                    "clip": "true"
-                }
-            }
-        },
-        "tags": {
-            "values": [
-            ],
-            "keyValues": {
-                "name": "Abq Rain",
-                "nwsOffice": "BOU"
-            }
-        }
-    }
-    schema_name = 'criteria_schema'
-    validator = get_validator(schema_name)
+def test_validate_criteria_message(criteria_validator, criteria_message):
     try:
-        validator.validate(message)
+        criteria_validator.validate(criteria_message)
     except ValidationError as exc:
         assert False, f'Validate message raised an exception {exc}'
 
 
-def test_validate_good_criteria_message():
-    message = {'corrId': {'originator': 'IDSSe',
-                          'issueDt': '_',
-                          'uuid': 'dc7ad8c1-5ff2-416f-9fee-66c598256189'},
-               'issueDt': '2018-11-13T20:20:39+00:00',
-               'validDt': {'start': '2018-11-13T21:20:39+00:00',
-                           'end': '2018-11-13T23:20:39+00:00'},
-               'tags': {'values': ['bob', 'uncle'], 'keyValues': {}},
-               'location': {'features': [{'type': 'Feature',
-                                          'properties': {'innerRadius': 3,
-                                                         'outerRadius': 4},
-                                          'geometry': {
-                                              'type': 'Point',
-                                              'coordinates': [
-                                                  -105.10392834544123,
-                                                  40.16676831094053]}}],
-                            'buffer': 1,
-                            'bufferUnits': 'miles'}}
-
-    schema_name = 'criteria_schema'
-    validator = get_validator(schema_name)
-    try:
-        validator.validate(message)
-    except ValidationError as exc:
-        assert False, f'Validate message raised an exception {exc}'
-
-
-def test_validate_bad_criteria_message():
-    message = {}
-
-    schema_name = 'criteria_schema'
-    validator = get_validator(schema_name)
-
+def test_validate_criteria_message_conditions(criteria_validator, criteria_message):
+    criteria_message.pop('conditions')
     with raises(ValidationError):
-        validator.validate(message)
+        criteria_validator.validate(criteria_message)
+
+
+def test_validate_criteria_message_with_missing_name(criteria_validator, criteria_message):
+    criteria_message['tags']['keyValues'].pop('name')
+    with raises(ValidationError):
+        criteria_validator.validate(criteria_message)
+
+
+def test_validate_criteria_message_with_bad_product_type(criteria_validator, criteria_message):
+    product = criteria_message['data']['A']['product']
+    product['not_fcst_or_obs'] = product.pop('fcst')
+    with raises(ValidationError):
+        criteria_validator.validate(criteria_message)
+
+
+def test_validate_criteria_message_with_bad_mapping(criteria_validator, criteria_message):
+    mapping = criteria_message['data']['B']['mapping']
+    mapping['smallest'] = mapping.pop('min')
+    with raises(ValidationError):
+        criteria_validator.validate(criteria_message)
 
 
 def test_validate_good_new_data_message():
