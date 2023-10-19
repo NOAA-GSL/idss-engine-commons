eb71f8cc3ad61d872976b497b08014392d0647d4
Mackenzie Grimes - NOAA Affiliate <mackenzie.grimes@noaa.gov>
2023-10-19 13:58:31 -0600

    merge branch 'main' into here

--- python/idsse_common/idsse/common/grid_proj.py
+++ python/idsse_common/idsse/common/grid_proj.py
@@ -6,8 +6,8 @@
 # Copyright (c) 2023 Colorado State University. All rights reserved. (2)
 #
 # Contributors:
-#    Mackenzie Grimes (2)
-#    Geary Layne (1)
+#     Mackenzie Grimes (2)
+#     Geary Layne (1)
 #
 # ----------------------------------------------------------------------------------
 # pylint: disable=invalid-name
