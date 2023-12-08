e9ba10ca994f01b4113dac321ab23d04e4b18bea
Geary-Layne <77741618+Geary-Layne@users.noreply.github.com>
2023-12-08 14:52:03 -0700

    Update test_validate_criteria_schema.py. removed print

--- python/idsse_common/test/test_validate_criteria_schema.py
+++ python/idsse_common/test/test_validate_criteria_schema.py
@@ -214,7 +214,6 @@ def test_validate_criteria_message_with_missing_name(criteria_validator: Validat
 def test_validate_criteria_message_with_bad_product_type(criteria_validator: Validator,
                                                          criteria_message: dict):
     product = criteria_message['parts'][0]['product']
-    print(product)
     product['not_fcst_or_obs'] = product.pop('fcst')
     with raises(ValidationError):
         criteria_validator.validate(criteria_message)
