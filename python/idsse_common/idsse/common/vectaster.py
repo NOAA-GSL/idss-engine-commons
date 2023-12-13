f67338a1abd8182b52283cf8bc5c8ce6ac0fc4ef
Geary-Layne <geary.j.layne@noaa.gov>
2023-12-13 16:26:11 -0700

    change approach for pixel_for_ and and support for point

--- python/idsse_common/idsse/common/vectaster.py
+++ python/idsse_common/idsse/common/vectaster.py
@@ -10,15 +10,19 @@
 # ----------------------------------------------------------------------------------
 
 import logging
-from math import ceil, floor
-from typing import Optional, Tuple, Union
+from math import floor
+from typing import List, Optional, Tuple, Union
 
 import numpy
-from shapely import contains_xy, Geometry, LineString, Point, Polygon, from_wkt
+from shapely import Geometry, LinearRing, LineString, Point, Polygon, from_wkt
 
 from idsse.common.grid_proj import GridProj, RoundingMethod
+from idsse.common.utils import round_half_away as round
 
 logger = logging.getLogger(__name__)
+
+
+Pixel = Tuple[int, int]
 
 
 def rasterize(
@@ -42,6 +46,11 @@ def rasterize(
     Returns:
         Tuple[numpy.array]: First array represent the x-coordinate and the seconde the y.
     """
+    if isinstance(geometry, str):
+        geometry = from_wkt(geometry)
+
+    if isinstance(geometry, Point):
+        return rasterize_point(geometry, grid_proj, rounding)
     if isinstance(geometry, LineString):
         return rasterize_linestring(geometry, grid_proj, rounding)
     if isinstance(geometry, Polygon):
@@ -49,8 +58,41 @@ def rasterize(
     raise ValueError(f'Passed geometry (type:{type(geometry)}) is not supported')
 
 
+def rasterize_point(
+        point: Union[str, Point],
+        grid_proj: GridProj = None,
+        rounding: RoundingMethod = RoundingMethod.FLOOR
+) -> Tuple[numpy.array]:
+    """Takes a geographic Point (specified with lon/lat) and determines the
+    associated pixel in the translated space (as specified by grid_proj).
+
+    Args:
+        point (Union[str, Point]): Either a shapely Point
+                                   or a well-known-text representation of a Point.
+        grid_proj (GridProj, optional): A projection (CRS) with scale and offsets. Defaults to None.
+        rounding (RoundingMethod, optional): Rounding role for mapping float coordinate values
+                                             to integers. Defaults to RoundingMethod.FLOOR.
+
+    Raises:
+        ValueError: When the line_string arg does not represent a Point
+
+    Returns:
+        Tuple[numpy.array]: First array represent the x-coordinate and the seconde the y.
+    """
+    if isinstance(point, str):
+        point = from_wkt(point)
+
+    if not isinstance(point, Point):
+        raise ValueError(f'Passed geometry is type:{type(point)} but must be Point')
+
+    if grid_proj is not None:
+        return _make_numpy([grid_proj.map_geo_to_pixel(*point.coords[0], rounding)])
+
+    return _make_numpy([_round_x_y(*point, rounding)])
+
+
 def rasterize_linestring(
-        line_string: Union[str, LineString],
+        linestring: Union[str, LineString],
         grid_proj: GridProj = None,
         rounding: RoundingMethod = RoundingMethod.FLOOR
 ) -> Tuple[numpy.array]:
@@ -58,8 +100,8 @@ def rasterize_linestring(
     associated pixels in the translated space (as specified by grid_proj).
 
     Args:
-        geometry (Union[str, LineString]): Either a shapely LineString
-                                         or a well-known-text representation of a LineString.
+        linestring (Union[str, LineString]): Either a shapely LineString
+                                             or a well-known-text representation of a LineString.
         grid_proj (GridProj, optional): A projection (CRS) with scale and offsets. Defaults to None.
         rounding (RoundingMethod, optional): Rounding role for mapping float coordinate values
                                              to integers. Defaults to RoundingMethod.FLOOR.
@@ -70,16 +112,16 @@ def rasterize_linestring(
     Returns:
         Tuple[numpy.array]: First array represent the x-coordinate and the seconde the y.
     """
-    if isinstance(line_string, str):
-        line_string = from_wkt(line_string)
+    if isinstance(linestring, str):
+        linestring = from_wkt(linestring)
 
-    if not isinstance(line_string, LineString):
-        raise ValueError(f'Passed geometry is type:{type(line_string)} but must be LineString')
+    if not isinstance(linestring, LineString):
+        raise ValueError(f'Passed geometry is type:{type(linestring)} but must be LineString')
 
     if grid_proj is not None:
-        line_string = geographic_linestring_to_pixel(line_string, grid_proj, rounding)
+        linestring = geographic_linestring_to_pixel(linestring, grid_proj, rounding)
 
-    return pixels_near_line_string(line_string)
+    return pixels_for_linestring(linestring)
 
 
 def rasterize_polygon(
@@ -187,7 +229,6 @@ def geographic_polygon_to_pixel(
     if not isinstance(poly, Polygon):
         raise ValueError(f'Geometry must be a Polygon but is a {type(poly)}')
 
-    print(grid_proj)
     exterior = [grid_proj.map_geo_to_pixel(*ll, rounding) for ll in poly.exterior.coords]
     interiors = [[grid_proj.map_geo_to_pixel(*ll, rounding) for ll in interior.coords]
                  for interior in poly.interiors]
@@ -196,59 +237,159 @@ def geographic_polygon_to_pixel(
     return pixel_poly
 
 
-def pixels_near_line_string(line_string: LineString,
-                            dist: float = 0.25) -> Tuple[numpy.array]:
-    """Determine the pixels that are close to a specified LineString
+def pixels_for_linestring(linestring: LineString) -> Tuple[numpy.array]:
+    """Determine the pixels that represent the specified LineString
 
     Args:
-        line_string (LineString): Shapely geometry with vertices defined by x,y
-        dist (float, optional): Distance cutoff used to determine in a pixel is "near".
-                                Defaults to 0.25.
+        linestring (LineString): Shapely geometry with vertices defined by x,y in pixel space
 
     Returns:
         Tuple[numpy.array]: First array represent the x-coordinate and the seconde the y.
     """
-    xmin, ymin, xmax, ymax = line_string.bounds
-    xmin = int(floor(xmin))
-    xmax = int(ceil(xmax))
-    ymin = int(floor(ymin))
-    ymax = int(ceil(ymax))
-
-    points = []
-    for y_coord in range(ymin, ymax):
-        for x_coord in range(xmin, xmax):
-            points.append(Point(x_coord, y_coord))
-
-    points_near_line_string = [(pnt.x, pnt.y) for pnt in points if line_string.distance(pnt) < dist]
-    return tuple(numpy.array(dim_coord) for dim_coord in map(list, zip(*points_near_line_string)))
+    return _make_numpy(_pixels_for_linestring(linestring))
 
 
 def pixels_in_polygon(poly: Polygon) -> Tuple[numpy.ndarray]:
-    """Determine the pixels that are within the specified Polygon
+    """Determine the pixels that represent the specified Polygon
 
     Args:
-        poly (Polygon): Shapely geometry with vertices defined by x,y
+        poly (Polygon): Shapely geometry with vertices defined by x,y in pixel space
 
     Returns:
         Tuple[numpy.array]: First array represent the x-coordinate and the seconde the y.
     """
-    xmin, ymin, xmax, ymax = poly.bounds
-    xmin = int(floor(xmin))
-    xmax = int(ceil(xmax))
-    ymin = int(floor(ymin))
-    ymax = int(ceil(ymax))
+    pixels = _pixels_for_polygon(poly.exterior)
+    for inner in poly.interiors:
+        pixels_on_inner_edge = _pixels_for_linestring(inner)
+        for pixel in _pixels_for_polygon(inner):
+            if pixel not in pixels_on_inner_edge:
+                pixels.remove(pixel)
 
-    # create prepared polygon
-    # prep_poly = prep(poly)
+    return _make_numpy(pixels)
 
-    # construct a rectangular mesh
-    points = []
-    for y_coord in range(ymin, ymax):
-        for x_coord in range(xmin, xmax):
-            points.append((x_coord, y_coord))
 
-    # validate if each point falls inside shape using
-    # the prepared polygon
-    # points_in_poly = list(filter(prep_poly.contains, points))
-    points_in_poly = [pnt for pnt in points if contains_xy(poly, *pnt)]
-    return tuple(numpy.array(dim_coord) for dim_coord in map(list, zip(*points_in_poly)))
+def _make_numpy(points: List[Pixel]) -> Tuple[numpy.ndarray]:
+    """Map a list of (x,y) tuples to a tuple of x values (as numpy array) and y values (numpy array)
+
+    Args:
+        points (List[Pixel]): list of (x,y) coordinates
+
+    Returns:
+        Tuple[numpy.ndarray]: Same coordinates but restructured
+    """
+    return tuple(numpy.array(dim_coord, dtype=numpy.int64) for dim_coord in map(list, zip(*points)))
+
+
+def _pixels_for_linestring(
+        linestring: LineString
+) -> List[Pixel]:
+    """Get pixels crossed while traversing a linestring
+
+    Args:
+        linestring (LineString): A linestring made of one or more line segments
+
+    Returns:
+        List[Pixel]: List of x,y tuples in pixel space
+    """
+    pixels = _pixels_for_line_seg(linestring.coords[0], linestring.coords[1])
+    for pnt1, pnt2 in zip(linestring.coords[1:-1], linestring.coords[2:]):
+        pixels.extend(_pixels_for_line_seg(pnt1, pnt2, exclude_first=True))
+    return pixels
+
+
+def _pixels_for_line_seg(
+        pnt1: Tuple[float, float],
+        pnt2: Tuple[float, float],
+        exclude_first: bool = False
+) -> List[Pixel]:
+    """Get pixels crossed while traversing a line segment
+
+    Args:
+        pnt1 (Tuple[float, float]): One of the line segment end points in x,y
+        pnt2 (Tuple[float, float]): The other line segment end point in x,y
+        exclude_first (bool): If true, pnt1 is not included in returned list
+
+    Returns:
+        List[Pixel]: List of x,y tuples in pixel space
+    """
+    x1, y1 = pnt1
+    x2, y2 = pnt2
+    dx = x2 - x1
+    dy = y2 - y1
+
+    pixels = []
+    if not exclude_first:
+        pixels.append((int(x1), int(y1)))
+
+    slope = float(dy) / float(dx) if dx else 0
+    if dx != 0 and -1 <= slope <= 1:
+        intercept = y1 - slope * x1
+        dx = 1 if x2 > x1 else -1
+        while (x1 != x2):
+            x1 += dx
+            y1 = round(slope * x1 + intercept)
+            pixels.append((int(x1), y1))
+    else:
+        slope = float(dx) / float(dy) if dy else 0
+        intercept = x1 - slope * y1
+        dy = 1 if y2 > y1 else -1
+        while (y1 != y2):
+            y1 += dy
+            x1 = round(slope * y1 + intercept)
+            pixels.append((x1, int(y1)))
+
+    return pixels
+
+
+def _pixels_for_polygon(
+        polygon_boundary: LinearRing
+) -> List[Pixel]:
+    """Get all pixels in a polygon using a line scan algorithm
+
+    Args:
+        polygon_boundary (LineString): The outside of a polygon as linestring, must
+                                       repeat first vertex as last, likely generated by
+                                       call the polygon.boundary property
+    Returns:
+        List[Pixel]: List of x,y tuples in pixel space
+    """
+    xmin, ymin, _, ymax = polygon_boundary.bounds
+    x_offset = 0 if xmin >= 0 else int(1-xmin)
+    ymin = floor(ymin)
+    ymax = floor(ymax)
+
+    edge_info = []
+    for (x1, y1), (x2, y2) in zip(polygon_boundary.coords[:-1], polygon_boundary.coords[1:]):
+        int_y1, int_y2 = int(y1), int(y2)
+        if int_y1 != int_y2:  # only need to include if y values differ
+            if int_y1 > int_y2:  # need to swap order so y1 < y2
+                x1, x2 = x2, x1
+                y1, y2 = y2, y1
+                int_y1, int_y2 = int_y2, int_y1
+
+            edge_info.append((int_y1, int_y2, x1+x_offset, (x2 - x1)/(y2 - y1)))
+
+    pixels = []
+    for y in range(ymin, ymax+1):
+        x_list = []
+        for y1, y2, x, slope in edge_info:
+            if y1 <= y <= y2:
+                x_list.append(int(x + (y-y1) * slope))
+
+        x_list.sort()
+
+        for x1, x2 in zip(x_list[:-1], x_list[1:]):
+            pixels.extend([(x, y) for x in range(x1-x_offset, x2-x_offset+1)])
+
+    return pixels
+
+
+def _round_x_y(x, y, rounding: Union[str, RoundingMethod]) -> Pixel:
+    if isinstance(rounding, str):
+        rounding = RoundingMethod[rounding.upper()]
+
+    if rounding is RoundingMethod.ROUND:
+        return (round(x), round(y))
+    if rounding is RoundingMethod.FLOOR:
+        return (floor(x), floor(y))
+    return (int(x), int(y))
--- python/idsse_common/test/test_vectaster.py
+++ python/idsse_common/test/test_vectaster.py
@@ -22,8 +22,10 @@ from idsse.common.vectaster import (geographic_geometry_to_pixel,
                                     geographic_polygon_to_pixel,
                                     from_wkt,
                                     rasterize,
+                                    rasterize_point,
                                     rasterize_linestring,
                                     rasterize_polygon)
+
 
 EXAMPLE_PROJ_SPEC = '+proj=lcc +lat_0=25.0 +lon_0=-95.0 +lat_1=25.0 +r=6371200'
 EXAMPLE_GRID_SPEC = '+dx=2539.703 +dy=2539.703 +w=2345 +h=1597 +lat_ll=19.229 +lon_ll=-126.2766'
@@ -76,32 +78,57 @@ def test_geographic_to_pixel(monkeypatch: MonkeyPatch, grid_proj: GridProj):
     polygon_mock.assert_called_once()
 
 
+def test_rasterize_point(grid_proj: GridProj):
+    linestring = from_wkt('POINT (-100.5 30.5)')
+    pixels = ((numpy.array([1081]), numpy.array([347])))
+    result = rasterize_point(linestring, grid_proj)
+    numpy.testing.assert_array_equal(result, pixels)
+
+
 def test_rasterize_linestring(grid_proj: GridProj):
     linestring = from_wkt('LINESTRING (-100 30, -100.1 30.1, -100.2 30)')
-    pixels = (numpy.array([1091., 1092., 1098., 1094., 1096.]),
-              numpy.array([324., 325., 325., 328., 328.]))
+    pixels = (numpy.array([1099, 1098, 1097, 1097, 1096, 1095, 1094, 1093, 1093, 1092, 1091]),
+              numpy.array([324, 325, 326, 327, 328, 329, 328, 327, 326, 325, 324]))
     result = rasterize_linestring(linestring, grid_proj)
     numpy.testing.assert_array_equal(result, pixels)
 
 
 def test_rasterize_polygon(grid_proj: GridProj):
     poly = from_wkt('POLYGON ((-105 40, -105.1 40, -105.1 40.1, -105 40.1, -105 40))')
-    pixels = (numpy.array([939, 940, 939, 940, 939, 940, 939, 940]),
-              numpy.array([779, 779, 780, 780, 781, 781, 782, 782]))
+    pixels = (numpy.array([938, 939, 940, 941, 938, 939, 940, 941, 938, 939, 940,
+                           941, 938, 939, 940, 941, 938, 939, 940, 941, 941, 938]),
+              numpy.array([778, 778, 778, 778, 779, 779, 779, 779, 780, 780, 780,
+                           780, 781, 781, 781, 781, 782, 782, 782, 782, 782, 783]))
     result = rasterize_polygon(poly, grid_proj)
     numpy.testing.assert_array_equal(result, pixels)
 
 
+def test_rasterize_polygon_with_hole():
+    poly = from_wkt('POLYGON ((0 0, 0 5, 5 5, 5 0, 0 0), (1 1, 3 1, 3 4, 1 4, 1 1))')
+    pixels = (numpy.array([0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 3, 4, 5,
+                           0, 1, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]),
+              numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2,
+                           3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5]))
+    result = rasterize_polygon(poly)
+    numpy.testing.assert_array_equal(result, pixels)
+
+
 def test_rasterize(monkeypatch: MonkeyPatch, grid_proj: GridProj):
-    line_string = from_wkt('LINESTRING (-105 40, -110 40, -110 50)')
+    point = from_wkt('POINT (-105 40)')
+    linestring = from_wkt('LINESTRING (-105 40, -110 40, -110 50)')
     polygon = from_wkt('POLYGON ((-105 40, -110 40, -110 50, -105 50, -105 40))')
 
-    line_string_mock = Mock()
+    point_mock = Mock()
+    linestring_mock = Mock()
     polygon_mock = Mock()
-    monkeypatch.setattr('idsse.common.vectaster.rasterize_linestring', line_string_mock)
+    monkeypatch.setattr('idsse.common.vectaster.rasterize_point', point_mock)
+    monkeypatch.setattr('idsse.common.vectaster.rasterize_linestring', linestring_mock)
     monkeypatch.setattr('idsse.common.vectaster.rasterize_polygon', polygon_mock)
 
-    rasterize(line_string, grid_proj)
-    line_string_mock.assert_called_once()
+    rasterize(point, grid_proj)
+    point_mock.assert_called_once()
+    rasterize(linestring, grid_proj)
+
+    linestring_mock.assert_called_once()
     rasterize(polygon, grid_proj)
     polygon_mock.assert_called_once()
