c397b1425c19efbae4b3854774c585d35d7f61bd
Mackenzie Grimes - NOAA Affiliate <136493179+mackenzie-grimes-noaa@users.noreply.github.com>
2023-12-15 12:48:15 -0700

    setup.py cannot have packages not used by base python Dockerfile

--- python/idsse_common/setup.py
+++ python/idsse_common/setup.py
@@ -17,9 +17,6 @@ setup(name='idsse',
       install_requires=[
         'pint',
         'importlib_metadata',
-        'h5netcdf',
-        'netCDF4',
-        'numpy',
         'pika',
         'jsonschema'
       ],
