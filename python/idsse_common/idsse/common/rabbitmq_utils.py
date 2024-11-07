857afb397e1956c38a3684143d1ec6309b15b5f4
Geary-Layne <geary.j.layne@noaa.gov>
2024-11-07 13:11:02 -0700

    added loggers to Publisher/Consumer

--- python/idsse_common/idsse/common/rabbitmq_utils.py
+++ python/idsse_common/idsse/common/rabbitmq_utils.py
@@ -394,6 +394,7 @@ class Consumer(Thread):
 
     def run(self):
         _set_context(self.context)
+        logger = logging.getLogger(f'{__name__}::{self.__class__.__name__}')
         logger.info('Start Consuming...  (to stop press CTRL+C)')
         self.channel.start_consuming()
 
@@ -468,6 +469,7 @@ class Publisher(Thread):
 
     def run(self):
         _set_context(self.context)
+        logger = logging.getLogger(f'{__name__}::{self.__class__.__name__}')
         logger.info('Starting publisher')
         while self._is_running:
             if self.connection and self.connection.is_open:
