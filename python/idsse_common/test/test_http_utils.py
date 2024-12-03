21e0af162a6f4c27ef737bc8ad01af710e23c686
Paul Hamer <81647525+paulhamer-noaa@users.noreply.github.com>
2024-12-03 11:03:56 -0700

    Update python/idsse_common/test/test_http_utils.py
    Co-authored-by: Geary Layne <77741618+Geary-Layne@users.noreply.github.com>

--- python/idsse_common/test/test_http_utils.py
+++ python/idsse_common/test/test_http_utils.py
@@ -148,7 +148,6 @@ def test_get_valids_all(http_utils: HttpUtils, httpserver: HTTPServer):
     httpserver.expect_request(url).respond_with_data(EXAMPLE_RETURN, content_type="text/plain")
     result = http_utils.get_valids(EXAMPLE_ISSUE)
     assert len(result) == 0
-    #assert result[0] == (EXAMPLE_VALID, f'{EXAMPLE_URL}{EXAMPLE_PROD_DIR}{EXAMPLE_FILES[1]}')
 
 
 def test_get_valids_with_wildcards(http_utils_with_wild: HttpUtils, httpserver: HTTPServer):
