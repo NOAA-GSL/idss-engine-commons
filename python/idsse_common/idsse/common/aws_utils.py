8be2530a4409a80ae70ebc7541b17e21112545a5
Mackenzie Grimes - NOAA Affiliate <mackenzie.grimes@noaa.gov>
2026-08-27 13:39:40 -0600

    chore: remove s5cmd from AwsUtils, simply use aws CLI

--- python/idsse_common/idsse/common/aws_utils.py
+++ python/idsse_common/idsse/common/aws_utils.py
@@ -40,9 +40,6 @@ class AwsUtils(ProtocolUtils):
             path = path + "/"  # ensure a trailing slash, which is expected by S3
 
         try:
-            commands = ["s5cmd", "--no-sign-request", "ls", path]
-            commands_result = exec_cmd(commands)
-        except FileNotFoundError:
             commands = ["aws", "s3", "--no-sign-request", "ls", path]
             commands_result = exec_cmd(commands)
         except PermissionError:
@@ -51,59 +48,23 @@ class AwsUtils(ProtocolUtils):
             return [os.path.join(path, filename.split(" ")[-1]) for filename in commands_result]
         return [filename.split(" ")[-1] for filename in commands_result]
 
-    def cp(
-        self,
-        path: str,
-        dest: str,
-        concurrency: int | None = None,
-        chunk_size: int | None = None,
-        **kwargs,
-    ) -> bool:
+    def cp(self, path: str, dest: str, **kwargs) -> bool:
         """Execute a 'cp' on the AWS s3 bucket specified by path, dest. Attempts to use
-        [s5cmd](https://github.com/peak/s5cmd) to copy the file from S3 with parallelization,
-        but falls back to (slower) aws-cli if s5cmd is not installed or throws an error.
+        aws-cli, which can be slow but works.
 
         Args:
             path (str): Relative or Absolute path to the object to be copied
             dest (str): The destination location
-            concurrency (optional, int): Number of parallel threads for s5cmd to use to copy
-                the file down from AWS (maybe helpful to tweak for large files).
-                Default is None (s5cmd default).
-            chunk_size (optional, int): Size of chunks (in MB) for s5cmd to split up the source AWS
-                S3 file so it can download quicker with more threads.
-                Default is None (s5cmd default).
 
         Returns:
             bool: Returns True if copy is successful
         """
         try:
-            logger.debug(
-                "First attempt with s5cmd, concurrency: %d, chunk_size: %s",
-                concurrency,
-                chunk_size,
-            )
-            commands = ["s5cmd", "--no-sign-request", "cp"]
-
-            # if concurrency and/or chunk_size options were provided, append to s5cmd before paths
-            if concurrency:
-                commands += ["--concurrency", str(concurrency)]
-            if chunk_size:
-                commands += ["--part-size", str(chunk_size)]
-            commands += [path, dest]  # finish the command list with the src and destination
-
+            logger.debug("Second attempt with aws command line")
+            commands = ["aws", "s3", "--no-sign-request", "cp", path, dest]
             exec_cmd(commands)
             return True
-        except PermissionError:
-            return (
-                False  # in s5cmd, this means 404 FileNotFound; don't bother retrying with aws-cli
-            )
-        except FileNotFoundError:
-            try:
-                logger.debug("Second attempt with aws command line")
-                commands = ["aws", "s3", "--no-sign-request", "cp", path, dest]
-                exec_cmd(commands)
-                return True
-            except Exception:  # pylint: disable=broad-exception-caught
-                return False
-            finally:
-                pass
+        except Exception:  # pylint: disable=broad-exception-caught
+            return False
+        finally:
+            pass
